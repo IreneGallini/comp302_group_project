67feab86341c3e1c8c03f36e389de04ad0090dff
natrigueros <nayla22to@gmail.com>
2026-03-25 17:41:56 -0500

    dont need this

--- IMDBLoadData.sql
+++
@@ -1,8 +0,0 @@
--- Intert data into IMDB tables created in IMDBTables.sql
-
-LOAD DATA INFILE 'language.csv'
-INTO TABLE languages
-FIELDS TERMINATED BY ','
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS; -- Skips the first line if it contains headers
