951a38c5c336d3ff60477e3ede821cfcaba0cd0c
IreneGallini <igallini@macalester.edu>
2026-03-24 23:30:34 -0500

    added drop table statements

--- IMDBTables.sql
+++ IMDBTables.sql
@@ -1,204 +1,166 @@
 -- TODO: put tables in the right order to avoid FK conflicts
---STEP 1: drop tables
+-- DROP TABLES
+DROP TABLE IF EXISTS participation;
+DROP TABLE IF EXISTS person_profession;
+DROP TABLE IF EXISTS media_production;
+DROP TABLE IF EXISTS country_of_production;
+DROP TABLE IF EXISTS review;
+DROP TABLE IF EXISTS media_content_warning;
+DROP TABLE IF EXISTS media_genre;
+DROP TABLE IF EXISTS production_company;
+DROP TABLE IF EXISTS person;
+DROP TABLE IF EXISTS media;
+DROP TABLE IF EXISTS profession;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS content_warning;
+DROP TABLE IF EXISTS genre;
+DROP TABLE IF EXISTS language;
+DROP TABLE IF EXISTS country;
 
-
--- STEP 2: create tables
+-- CREATE TABLES
 --IRENE
 -- LANGUAGE TABLE
 CREATE TABLE language (
-    id SERIAL PRIMARY KEY,
+    id AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL UNIQUE
-);
+) ENGINE=InnoDB;
 
 -- GENRE TABLE
 CREATE TABLE genre (
-    id SERIAL PRIMARY KEY,
+    id AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL UNIQUE
-);
+) ENGINE=InnoDB;
 
 -- CONTENT WARNING TABLE
 CREATE TABLE content_warning (
-    id SERIAL PRIMARY KEY,
+    id AUTO_INCREMENT PRIMARY KEY,
     type VARCHAR(100) NOT NULL UNIQUE
-);
+) ENGINE=InnoDB;
 
 -- USER TABLE
-CREATE TABLE "user" (
+CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(100) NOT NULL UNIQUE,
     join_date DATE NOT NULL DEFAULT CURRENT_DATE
-);
+) ENGINE=InnoDB;
 
 -- MEDIA TABLE
 CREATE TABLE media (
-    id SERIAL PRIMARY KEY,
+    id AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     original_title VARCHAR(255),
     type VARCHAR(50) NOT NULL,
     language_id INT NOT NULL,
     budget NUMERIC(15,2) CHECK (budget >= 0),
 
-    CONSTRAINT fk_media_language
-        FOREIGN KEY (language_id)
-        REFERENCES language(id)
-        ON DELETE RESTRICT
-);
+    FOREIGN KEY (language_id) REFERENCES language(id) ON DELETE RESTRICT
+) ENGINE=InnoDB;
 
 -- MEDIA_GENRE (MANY-TO-MANY)
 CREATE TABLE media_genre (
-    media_id INT,
-    genre_id INT,
+    media_id INT NOT NULL,
+    genre_id INT NOT NULL,
 
     PRIMARY KEY (media_id, genre_id),
 
-    CONSTRAINT fk_media_genre_media
-        FOREIGN KEY (media_id)
-        REFERENCES media(id)
-        ON DELETE CASCADE,
-
-    CONSTRAINT fk_media_genre_genre
-        FOREIGN KEY (genre_id)
-        REFERENCES genre(id)
-        ON DELETE CASCADE
-);
+    FOREIGN KEY (media_id) REFERENCES media(id) ON DELETE CASCADE,
+    FOREIGN KEY (genre_id) REFERENCES genre(id) ON DELETE CASCADE
+) ENGINE=InnoDB;
 
 -- MEDIA_CONTENT_WARNING (MANY-TO-MANY)
 CREATE TABLE media_content_warning (
-    media_id INT,
-    warning_id INT,
+    media_id INT NOT NULL,
+    warning_id INT NOT NULL,
 
     PRIMARY KEY (media_id, warning_id),
 
-    CONSTRAINT fk_mc_media
-        FOREIGN KEY (media_id)
-        REFERENCES media(id)
-        ON DELETE CASCADE,
-
-    CONSTRAINT fk_mc_warning
-        FOREIGN KEY (warning_id)
-        REFERENCES content_warning(id)
-        ON DELETE CASCADE
-);
+    FOREIGN KEY (media_id) REFERENCES media(id) ON DELETE CASCADE,
+    FOREIGN KEY (warning_id) REFERENCES content_warning(id) ON DELETE CASCADE
+) ENGINE=InnoDB; 
 
 -- REVIEW TABLE
 CREATE TABLE review (
-    id SERIAL PRIMARY KEY,
+    id AUTO_INCREMENT PRIMARY KEY,
     media_id INT NOT NULL,
     user_id INT NOT NULL,
     score INT NOT NULL CHECK (score BETWEEN 1 AND 10),
     text TEXT,
     date_posted TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    CONSTRAINT fk_review_media
-        FOREIGN KEY (media_id)
-        REFERENCES media(id)
-        ON DELETE CASCADE,
-
-    CONSTRAINT fk_review_user
-        FOREIGN KEY (user_id)
-        REFERENCES "user"(id)
-        ON DELETE CASCADE,
-
-    CONSTRAINT unique_user_review
-        UNIQUE (media_id, user_id) -- one review per user per media
-);
+    FOREIGN KEY (media_id) REFERENCES media(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    UNIQUE (media_id, user_id) -- one review per user per media
+) ENGINE=InnoDB;
 
 -- SAM
--- LANGUAGE TABLE
-CREATE TABLE language (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL UNIQUE
-);
+CREATE TABLE participation
+(
+participation_id INT PRIMARY KEY,
+media_id INT,
+person_id INT,
+profession_id INT,
+character_id INT,
+FOREIGN KEY person_id REFERENCES person (id),
+FOREIGN KEY media_id REFERENCES media (id),
+FOREIGN KEY profession_id REFERENCES profession (id),
+FOREIGN KEY character_id REFERENCES character (id)
+) ENGINE = InnoDB;
 
--- GENRE TABLE
-CREATE TABLE genre (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL UNIQUE
-);
 
--- CONTENT WARNING TABLE
-CREATE TABLE content_warning (
-    id SERIAL PRIMARY KEY,
-    type VARCHAR(100) NOT NULL UNIQUE
-);
+CREATE TABLE person
+(
+person_id INT PRIMARY KEY,
+name VARCHAR(64),
+birth_year YEAR,
+death_year YEAR,
+nationality_id INT,
+FOREIGN KEY nationality_id REFERENCES country (id)
+) ENGINE = InnoDB;
 
--- USER TABLE
-CREATE TABLE "user" (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(100) NOT NULL UNIQUE,
-    join_date DATE NOT NULL DEFAULT CURRENT_DATE
-);
+CREATE TABLE person_profession
+(
+person_id INT,
+profession_id INT,
+PRIMARY KEY(person_id, profession_id),
+FOREIGN KEY person_id REFERENCES person (id),
+FOREIGN KEY profession_id REFERENCES profession (id)
+) ENGINE = InnoDB;
 
--- MEDIA TABLE
-CREATE TABLE media (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    original_title VARCHAR(255),
-    type VARCHAR(50) NOT NULL,
-    language_id INT NOT NULL,
-    budget NUMERIC(15,2) CHECK (budget >= 0),
+CREATE TABLE profession
+(
+id INT PRIMARY KEY,
+name VARCHAR(64)
+) ENGINE = InnoDB;
 
-    CONSTRAINT fk_media_language
-        FOREIGN KEY (language_id)
-        REFERENCES language(id)
-        ON DELETE RESTRICT
-);
+CREATE TABLE country_of_production
+(
+country_id INT,
+media_id INT,
+PRIMARY KEY (media_id, country_id),
+FOREIGN KEY media_id REFERENCES media (id),
+FOREIGN KEY country_id REFERENCES country (id)
+) ENGINE = InnoDB;
 
--- MEDIA_GENRE (MANY-TO-MANY)
-CREATE TABLE media_genre (
-    media_id INT,
-    genre_id INT,
+CREATE TABLE country
+(
+id INT PRIMARY KEY,
+name VARCHAR(64)
+) ENGINE = InnoDB;
 
-    PRIMARY KEY (media_id, genre_id),
+CREATE TABLE production_company
+(
+id INT PRIMARY KEY,
+name VARCHAR(64),
+hq_country_id INT,
+FOREIGN KEY hq_country_id REFERENCES country (id)
+) ENGINE = InnoDB;
 
-    CONSTRAINT fk_media_genre_media
-        FOREIGN KEY (media_id)
-        REFERENCES media(id)
-        ON DELETE CASCADE,
-
-    CONSTRAINT fk_media_genre_genre
-        FOREIGN KEY (genre_id)
-        REFERENCES genre(id)
-        ON DELETE CASCADE
-);
-
--- MEDIA_CONTENT_WARNING (MANY-TO-MANY)
-CREATE TABLE media_content_warning (
-    media_id INT,
-    warning_id INT,
-
-    PRIMARY KEY (media_id, warning_id),
-
-    CONSTRAINT fk_mc_media
-        FOREIGN KEY (media_id)
-        REFERENCES media(id)
-        ON DELETE CASCADE,
-
-    CONSTRAINT fk_mc_warning
-        FOREIGN KEY (warning_id)
-        REFERENCES content_warning(id)
-        ON DELETE CASCADE
-);
-
--- REVIEW TABLE
-CREATE TABLE review (
-    id SERIAL PRIMARY KEY,
-    media_id INT NOT NULL,
-    user_id INT NOT NULL,
-    score INT NOT NULL CHECK (score BETWEEN 1 AND 10),
-    text TEXT,
-    date_posted TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT fk_review_media
-        FOREIGN KEY (media_id)
-        REFERENCES media(id)
-        ON DELETE CASCADE,
-
-    CONSTRAINT fk_review_user
-        FOREIGN KEY (user_id)
-        REFERENCES "user"(id)
-        ON DELETE CASCADE,
-
-    CONSTRAINT unique_user_review
-        UNIQUE (media_id, user_id) -- one review per user per media
-);
+CREATE TABLE media_production
+(
+media_id INT,
+company_id INT,
+role VARCHAR(64),
+PRIMARY KEY(media_id, company_id),
+FOREIGN KEY media_id REFERENCES media (id),
+FOREIGN KEY company_id REFERENCES production_company (id)
+) ENGINE = InnoDB;
