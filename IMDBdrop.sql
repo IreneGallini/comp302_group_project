08b4e22e9bc3e0900cb5834460cb46125687bb68
IreneGallini <igallini@macalester.edu>
2026-03-26 10:05:57 -0500

    added drop script

---
+++ IMDBdrop.sql
@@ -0,0 +1,25 @@
+DROP TABLE IF EXISTS award_nomination;
+DROP TABLE IF EXISTS domestic_box_office;
+DROP TABLE IF EXISTS episode;
+DROP TABLE IF EXISTS movie;
+DROP TABLE IF EXISTS participation;
+DROP TABLE IF EXISTS person_profession;
+DROP TABLE IF EXISTS media_production;
+DROP TABLE IF EXISTS country_of_production;
+DROP TABLE IF EXISTS review;
+DROP TABLE IF EXISTS media_content_warning;
+DROP TABLE IF EXISTS media_genre;
+
+DROP TABLE IF EXISTS series;
+DROP TABLE IF EXISTS production_company;
+DROP TABLE IF EXISTS characters;
+DROP TABLE IF EXISTS person;
+
+DROP TABLE IF EXISTS profession;
+DROP TABLE IF EXISTS award;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS content_warning;
+DROP TABLE IF EXISTS genre;
+DROP TABLE IF EXISTS media;
+DROP TABLE IF EXISTS languages;
+DROP TABLE IF EXISTS country;
